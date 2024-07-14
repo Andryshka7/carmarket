611fbdf38a6aee0d85d1a48c9b5f11e5e7ee411e
Andryshka16 <a.lapchik7@gmail.com>
2024-07-14 20:01:38 +0300

    fixed delete files function

--- server/src/helpers/delete files/index.ts
+++ server/src/helpers/delete files/index.ts
@@ -1,10 +1,12 @@
+import { SERVER_URL } from 'config'
 import { unlink } from 'fs/promises'
 import { join } from 'path'
 
-const deleteFiles = async (...files: string[]) => {
+const deleteFiles = async (...urls: string[]) => {
 	await Promise.all(
-		files.map((fileName) => {
-			const filePath = join(__dirname, '..', '..', '..', fileName)
+		urls.map((url) => {
+			const fileName = url.replace(`${SERVER_URL}/images/`, '')
+			const filePath = join(__dirname, '..', '..', '..', 'images', fileName)
 			return unlink(filePath)
 		})
 	)
